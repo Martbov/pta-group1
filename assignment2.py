efb2c43e67ef808ac3ac17ffa378e516e7c10990
Martbov <Mart@RN-145-97-166-241.eduroam.rug.nl>
2015-04-22 13:24:49 +0200

    Assignment 2 part 1 finished

--- assignment2.py
+++ assignment2.py
@@ -1,11 +1,31 @@
 #!usr/bin/python2.7
 
-from nltk.corpus import brown 
+from nltk.corpus import brown, treebank
 from nltk.tag import UnigramTagger
+from nltk import pos_tag
+
+def main():
+	sent = ['Marley', 'was', 'dead', ':', 'to', 'begin', 'with', '.', 'There', 'is', 'no', 'doubt', 'whatever', 'about', 'that', '.']
+
+	# Deel 1
+	"""print("Brown tagger:")
+	brownTagger = UnigramTagger(brown.tagged_sents())
+	for word, tag in brownTagger.tag(sent):
+		print(word,'->',tag)
+
+	print("PENN Treebank Tagger:")
+	pennTagger = UnigramTagger(treebank.tagged_sents())
+	for word, tag in pennTagger.tag(sent):
+		print(word, '->', tag)
+
+	print("NLTK tagger:")
+	print(pos_tag(sent))"""
+
+	# Deel 2
+
 	
-tagger = UnigramTagger(brown.tagged_sents(categories='news')[:500])
-sent = ['Marley', 'was', 'dead', ':', 'to', 'begin', 'with', '.', 'There', 'is', 'no', 'doubt', 'whatever', 'about', 'that', '.']
-for word, tag in tagger.tag(sent):
-	print(word,'->',tag)
 
 
+
+if __name__ == '__main__':
+	main()
