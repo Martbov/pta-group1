80d030989ad35be87c8875b066686b11867b958b
S2814714 <s2814714@let-edu-145-74.let.rug.nl>
2015-04-22 12:48:37 +0200

    Opdracht 1 af

--- assignment1.py
+++ assignment1.py
@@ -3,6 +3,7 @@
 import nltk
 from nltk.collocations import *
 
+
 def most_common(countList):
 	listFreq = nltk.FreqDist(countList)
 	return listFreq.most_common(20)
@@ -22,17 +23,21 @@ def main():
 	trigramChars = nltk.trigrams(charList)
 
 	#print("Answer to 1C: The 20 most common characters are: \nUnigrams: \n{}\nBigrams: \n{}\nTrigrams: \n{}".format(most_common(charList), 
-		most_common(bigramChars), most_common(trigramChars)))
+		#most_common(bigramChars), most_common(trigramChars)))
 
 	bigramWords = nltk.bigrams(tokens)
 	trigramWords = nltk.trigrams(tokens)
 
 	#print("Answer to 1D: The 20 most common words are: \nUnigrams: \n{}\nBigrams: \n{}\nTrigrams: \n{}".format(most_common(tokens), 
-		most_common(bigramWords), most_common(trigramWords)))
-
-
+		#most_common(bigramWords), most_common(trigramWords)))
 	
-
+	bigram_measures = nltk.collocations.BigramAssocMeasures()
+	finder = BigramCollocationFinder.from_words(tokens)
+	scoredPMI = finder.score_ngrams(bigram_measures.pmi)
+	scoredCHI = finder.score_ngrams(bigram_measures.chi_sq)
+	#print("Answer to 2: The 20 most likely collocations are:\n PMI:\n {} \n Chi's square\n {}" .format(scoredPMI[:20],scoredCHI[:20]))
+	
+	print("Speermans correlation = {}".format(nltk.metrics.spearman.spearman_correlation(scoredPMI, scoredCHI)))
 
 if __name__ == '__main__':
 	main()
