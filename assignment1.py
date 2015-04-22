7825e55b67df8724f96add92647b47d2ffd375bb
Martbov <Mart@RN-145-97-166-241.eduroam.rug.nl>
2015-04-22 12:23:49 +0200

    Finished assignment1.1

--- assignment1.py
+++ assignment1.py
@@ -1,7 +1,11 @@
-#!usr/bin/python2.7
+#!usr/bin/python3.4
 
 import nltk
 from nltk.collocations import *
+
+def most_common(countList):
+	listFreq = nltk.FreqDist(countList)
+	return listFreq.most_common(20)
 
 def main():
 	text = open('holmes.txt').read()
@@ -12,14 +16,22 @@ def main():
 			charList.append(char)
 	fDistChars = nltk.FreqDist(charList)
 	fDistWords = nltk.FreqDist(tokens)
-	#print("Answer to A: There are {} character types in the book, namely: {}".format(len(fDistChars),sorted(fDistChars)))
-	#print("Answer to B: There are {} word types in the book, namely: {}".format(len(fDistWords),sorted(fDistWords)))
-	#print("Answer to C: The 20 most common unigram characters are: \n")
-	unigramList = []
-	for u,f in fDistChars.items():
-			unigramList.append((u,f))
-	#print(unigramList[:20])
-	print(fDistChars.most_common(20))
+	#print("Answer to 1A: There are {} character types in the book, namely: {}".format(len(fDistChars),sorted(fDistChars)))
+	#print("Answer to 1B: There are {} word types in the book, namely: {}".format(len(fDistWords),sorted(fDistWords)))
+	bigramChars = nltk.bigrams(charList)
+	trigramChars = nltk.trigrams(charList)
+
+	#print("Answer to 1C: The 20 most common characters are: \nUnigrams: \n{}\nBigrams: \n{}\nTrigrams: \n{}".format(most_common(charList), 
+		most_common(bigramChars), most_common(trigramChars)))
+
+	bigramWords = nltk.bigrams(tokens)
+	trigramWords = nltk.trigrams(tokens)
+
+	#print("Answer to 1D: The 20 most common words are: \nUnigrams: \n{}\nBigrams: \n{}\nTrigrams: \n{}".format(most_common(tokens), 
+		most_common(bigramWords), most_common(trigramWords)))
+
+
+	
 
 
 if __name__ == '__main__':
