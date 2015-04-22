86dda2900004d3320f36a0893a6837cd004f8af2
Martbov <Mart@RN-145-97-166-241.eduroam.rug.nl>
2015-04-22 11:10:25 +0200

    Added assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3.4
+
+import nltk
+from nltk.collocations import *
+
+def main():
+	text = open('holmes.txt').read()
+ 	tokens = nltk.wordpunct_tokenize(text)
+	bigram_measures = nltk.collocations.BigramAssocMeasures() 6 >>> finder = BigramCollocationFinder.from_words(tokens)
+	scored = finder.score_ngrams(bigram_measures.raw_freq)
+	sorteD = sorted(bigram for bigram, score in scored)
+	print(sorted)
+
+if __name__ == '__main__':
+	main()
