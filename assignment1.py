05aefe037e9461d4e815f4f32b0db46e406d7d62
S2814714 <s2814714@let-edu-145-74.let.rug.nl>
2015-04-22 12:05:32 +0200

    C klaar

--- assignment1.py
+++ assignment1.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3.4
+#!usr/bin/python2.7
 
 import nltk
 from nltk.collocations import *
@@ -12,12 +12,15 @@ def main():
 			charList.append(char)
 	fDistChars = nltk.FreqDist(charList)
 	fDistWords = nltk.FreqDist(tokens)
-	print("Answer to A: There are {} character types in the book, namely: {}".format(len(fDistChars),sorted(fDistChars)))
-	print("Answer to B: There are {} word types in the book, namely: {}".format(len(fDistWords),sorted(fDistWords)))
-	bigram_measures = nltk.collocations.BigramAssocMeasures()
-	finder = BigramCollocationFinder.from_words(tokens)
-	scored = finder.score_ngrams(bigram_measures.raw_freq)
-	sorteD = sorted(bigram for bigram, score in scored)
+	#print("Answer to A: There are {} character types in the book, namely: {}".format(len(fDistChars),sorted(fDistChars)))
+	#print("Answer to B: There are {} word types in the book, namely: {}".format(len(fDistWords),sorted(fDistWords)))
+	#print("Answer to C: The 20 most common unigram characters are: \n")
+	unigramList = []
+	for u,f in fDistChars.items():
+			unigramList.append((u,f))
+	#print(unigramList[:20])
+	print(fDistChars.most_common(20))
+
 
 if __name__ == '__main__':
 	main()
