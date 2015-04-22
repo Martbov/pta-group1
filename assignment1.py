96a7e410f51309294a46964d4566360b6f18431a
S2814714 <s2814714@let-edu-145-74.let.rug.nl>
2015-04-22 11:31:29 +0200

    Opdracht 1 af

--- assignment1.py
+++ assignment1.py
@@ -6,11 +6,18 @@ from nltk.collocations import *
 def main():
 	text = open('holmes.txt').read()
 	tokens = nltk.wordpunct_tokenize(text)
+	charList = []
+	for word in tokens:
+		for char in word:
+			charList.append(char)
+	#print(charList)
+	fdist=nltk.FreqDist(charList)
+	print("There are {} character types in the book".format(len(fdist)))
 	bigram_measures = nltk.collocations.BigramAssocMeasures()
 	finder = BigramCollocationFinder.from_words(tokens)
 	scored = finder.score_ngrams(bigram_measures.raw_freq)
 	sorteD = sorted(bigram for bigram, score in scored)
-	print(sorteD)
+	#print(tokens)
 
 if __name__ == '__main__':
 	main()
