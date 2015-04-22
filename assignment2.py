6674b27fb4e1767751011cbbcc02582412820ae8
Martbov <Mart@RN-145-97-166-241.eduroam.rug.nl>
2015-04-22 13:48:29 +0200

    Started part 2

--- assignment2.py
+++ assignment2.py
@@ -3,6 +3,7 @@
 from nltk.corpus import brown, treebank
 from nltk.tag import UnigramTagger
 from nltk import pos_tag
+from collections import Counter
 
 def main():
 	sent = ['Marley', 'was', 'dead', ':', 'to', 'begin', 'with', '.', 'There', 'is', 'no', 'doubt', 'whatever', 'about', 'that', '.']
@@ -23,7 +24,21 @@ def main():
 
 	# Deel 2
 
+	br_tw = brown.tagged_words(categories='mystery')
+	br_ts = brown.tagged_sents(categories='mystery')
+
+	#print("Answer to 2A: \nWords: {} \nSentences: {}".format(len(br_tw), len(br_ts)))
+	#print("Answer to 2B: \n100th word: {} \n101th word: {}".format(br_tw[99][0], br_tw[100][0]))
 	
+	c = Counter()
+	for word, tag in br_tw:
+		c.update(tag)
+	print(c)
+		
+
+	"""brownTagger = UnigramTagger(brown.tagged_sents())
+	for word, tag in brownTagger.tag(sent):
+		print(word,'->',tag)"""
 
 
 
