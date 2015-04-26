400c972819b4ee4a05f241f468de84cad4dc5e00
Martbov <Mart@MacBook-Pro-van-Mart.local>
2015-04-26 18:12:52 +0200

    Cleaned up the code and added pdf file with output

--- assignment1.py
+++ assignment1.py
@@ -17,27 +17,30 @@ def main():
 			charList.append(char)
 	fDistChars = nltk.FreqDist(charList)
 	fDistWords = nltk.FreqDist(tokens)
-	#print("Answer to 1A: There are {} character types in the book, namely: {}".format(len(fDistChars),sorted(fDistChars)))
-	#print("Answer to 1B: There are {} word types in the book, namely: {}".format(len(fDistWords),sorted(fDistWords)))
+	
+	print("Answer to 1A, there are {} character types in the book, namely: \n{}".format(len(fDistChars),sorted(fDistChars)))
+	print("\nAnswer to 1B, there are {} word types in the book, namely: \n{}".format(len(fDistWords),sorted(fDistWords)))
+	
 	bigramChars = nltk.bigrams(charList)
 	trigramChars = nltk.trigrams(charList)
 
-	#print("Answer to 1C: The 20 most common characters are: \nUnigrams: \n{}\nBigrams: \n{}\nTrigrams: \n{}".format(most_common(charList), 
-		#most_common(bigramChars), most_common(trigramChars)))
+	print("\nAnswer to 1C, the 20 most common characters are: \nUnigrams: \n{}\nBigrams: \n{}\nTrigrams: \n{}".format(most_common(charList), 
+		most_common(bigramChars), most_common(trigramChars)))
 
 	bigramWords = nltk.bigrams(tokens)
 	trigramWords = nltk.trigrams(tokens)
 
-	#print("Answer to 1D: The 20 most common words are: \nUnigrams: \n{}\nBigrams: \n{}\nTrigrams: \n{}".format(most_common(tokens), 
-		#most_common(bigramWords), most_common(trigramWords)))
+	print("\nAnswer to 1D, the 20 most common words are: \nUnigrams: \n{}\nBigrams: \n{}\nTrigrams: \n{}".format(most_common(tokens), 
+		most_common(bigramWords), most_common(trigramWords)))
 	
 	bigram_measures = nltk.collocations.BigramAssocMeasures()
 	finder = BigramCollocationFinder.from_words(tokens)
 	scoredPMI = finder.score_ngrams(bigram_measures.pmi)
 	scoredCHI = finder.score_ngrams(bigram_measures.chi_sq)
-	#print("Answer to 2: The 20 most likely collocations are:\n PMI:\n {} \n Chi's square\n {}" .format(scoredPMI[:20],scoredCHI[:20]))
 	
-	print("Spearmans correlation = {}".format(nltk.metrics.spearman.spearman_correlation(scoredPMI, scoredCHI)))
+	print("\nAnswer to 2, the 20 most likely collocations are:\nPMI:\n{} \nChi's square\n{}" .format(scoredPMI[:20],scoredCHI[:20]))
+	
+	print("\nSpearmans correlation = {}".format(nltk.metrics.spearman.spearman_correlation(scoredPMI, scoredCHI)))
 
 if __name__ == '__main__':
 	main()
