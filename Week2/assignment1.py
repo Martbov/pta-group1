980cafc7f728700bb440973bb0eaf3d0a25104af
Martbov <Mart@RN-145-97-166-128.eduroam.rug.nl>
2015-04-30 10:07:32 +0200

    Progress update

--- Week2/assignment1.py
+++ Week2/assignment1.py
@@ -3,7 +3,7 @@
 import nltk
 from nltk import pos_tag
 from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.corpus import wordnet
+from nltk.corpus import wordnet as wn
 
 def tokenize(textfile):
 	textFile = open(textfile, 'r')
@@ -11,11 +11,36 @@ def tokenize(textfile):
 	textFile.close()
 	sents = nltk.sent_tokenize(rawText)
 	tokens = []
+	nounTokens = []
 	for sent in sents:
 		tokens += nltk.word_tokenize(sent)
-	pos_tags = pos_tag(tokens)
-	return tokens
+		pos_tags = pos_tag(tokens)
+		for token in pos_tags:
+			if token[1] == 'NN' or token[1] == 'NNP' or token[1] == 'NNS' or token[1] == 'NNPS':
+				nounTokens.append(token[0])
+	return nounTokens
 
 def lemmatize(tokens):
 	lemmatizer = WordNetLemmatizer()
-	lemmatizer.lemmatize(lemma, wordnet.NOUN)
+	nounLemmas = []
+	for token in tokens:
+		nounLemmas.append(lemmatizer.lemmatize(token, wn.NOUN))
+	return nounLemmas
+
+def synsets(nounList):
+	synsetList=[]
+	for noun in nounList:
+		synsetList.append(wn.synsets('noun'))
+	for synset in synsetList:
+		print(synset[0].name())
+	return synsetList
+
+
+def main():
+	infile = 'ada_lovelace.txt'
+	tokens = tokenize(infile)
+	nouns = lemmatize(tokens)
+	synsets(nouns)
+
+if __name__ == '__main__':
+	main()
