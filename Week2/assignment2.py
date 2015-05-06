919d1adde0ba680446e7543217b48fd0060fb18a
Olivier Louwaars <o_louwaars@hotmail.com>
2015-05-06 17:27:33 +0200

    dingen toegevoegd

--- Week2/assignment2.py
+++ Week2/assignment2.py
@@ -1,10 +1,36 @@
 #!usr/bin/python3.4
-
+import nltk
+from collections import Counter
+from nltk.tag.stanford import NERTagger
 from nltk.corpus import wordnet as wn
+import os
+import codecs
 
 
 def main():
-	print(wn.synsets('dog'))
+	os.environ['JAVAHOME'] = "C:\Program Files\Java\jdk1.8.0_45/bin"
+	path="C:\Users\Olivier\Copy\Premaster Information Science\PTA\pta-group1\Week2\stanford-ner-2014-06-16"
+	classifier = path + "/classifiers/" + "english.muc.7class.distsim.crf.ser.gz"
+	jar = path + "/stanford-ner-3.4.jar"
+	tagger = NERTagger(classifier, jar)
+
+	textFile = codecs.open('ada_lovelace.txt', 'r','windows-1252')
+	rawText = textFile.read()
+	textFile.close()
+	sents = nltk.sent_tokenize(rawText)
+	tokens = []
+	for sent in sents:
+		tokens += nltk.word_tokenize(sent)
+
+	taggedText = tagger.tag(tokens)
+
+	countList=[]
+	for taggedSent in taggedText:
+		for taggedWord in taggedSent:
+			countList.append(taggedWord[1])
+	print("Answer to 2.1: \n{} \nThey certainly aren't all correct.".format(Counter(countList)))
+	print()
+	print("Answer to 2.2: The other classifiers seem to achieve similar results,\nbut because of the multiple categories it is more interesting to read.")
 
 
 if __name__ == '__main__':
