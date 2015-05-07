0e753185100f8f40083ee76ea00fb2a54581eadd
Martbov <Mart@MacBook-Pro-van-Mart.local>
2015-05-07 13:55:33 +0200

    Assignment 2 almost finished

--- Week2/assignment2.py
+++ Week2/assignment2.py
@@ -5,28 +5,58 @@ from nltk.tag.stanford import NERTagger
 from nltk.corpus import wordnet as wn
 import os
 import codecs
-from assignment1 import *
+from nltk import pos_tag
+from nltk.stem.wordnet import WordNetLemmatizer
+
+def tokenize(textfile):
+	textFile = open(textfile, 'r')
+	rawText = textFile.read()
+	textFile.close()
+	sents = nltk.sent_tokenize(rawText)
+	tokens = []
+	nounTokens = []
+	for sent in sents:
+		tokens += nltk.word_tokenize(sent)
+		pos_tags = pos_tag(tokens)
+		for token in pos_tags:
+			if token[1] == 'NN' or token[1] == 'NNP' or token[1] == 'NNS' or token[1] == 'NNPS':
+				nounTokens.append(token[0])
+	return nounTokens
+
+def lemmatize(tokens):
+	lemmatizer = WordNetLemmatizer()
+	nounLemmas = []
+	for token in tokens:
+		nounLemmas.append(lemmatizer.lemmatize(token, wn.NOUN))
+	return nounLemmas
 
 def main():
-	os.environ['JAVAHOME'] = "C:\Program Files\Java\jdk1.8.0_45/bin"
-	path="C:\Users\Olivier\Copy\Premaster Information Science\PTA\pta-group1\Week2\stanford-ner-2014-06-16"
+	#os.environ['JAVAHOME'] = "C:\Program Files\Java\jdk1.8.0_45/bin"
+	path="ner"
 	classifier = path + "/classifiers/" + "english.muc.7class.distsim.crf.ser.gz"
 	jar = path + "/stanford-ner-3.4.jar"
 	tagger = NERTagger(classifier, jar)
 
-	tokens=tokenize('ada_lovelace.txt')
+	tokens = tokenize('ada_lovelace.txt')
+	#print(tokens)
 
-	#taggedText = tagger.tag(tokens)
+	taggedText = tagger.tag(tokens)
+	
 
 	countList=[]
-	for taggedSent in taggedText:
-		for taggedWord in taggedSent:
-			countList.append(taggedWord[1])
-			print(taggedWord)
+	nounList = []
+	for word, tag in taggedText:
+		if tag != 'O':
+			countList.append(tag)
+			nounList.append(word)
+
+	
 	#print("Answer to 2.1: \n{} \nThey certainly aren't all correct.".format(Counter(countList)))
 	#print()
 	#print("Answer to 2.2: The other classifiers seem to achieve similar results,\nbut because of the multiple categories it is more interesting to read.")
 
+	lemmas = lemmatize(nounList)
+	print(lemmas)
 
 
 
