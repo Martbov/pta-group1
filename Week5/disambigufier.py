36de36d8a602333439f642ca396a3730a2b725d2
Olivier Louwaars <o_louwaars@hotmail.com>
2015-05-23 14:59:17 +0200

    jeee

--- Week5/disambigufier.py
+++ Week5/disambigufier.py
@@ -1,7 +1,7 @@
 import pickle, nltk, re
 from nltk import pos_tag, word_tokenize, sent_tokenize
 from nltk.wsd import lesk
-from nltk.corpus import wordnet
+from nltk.corpus import wordnet as wn
 
 def main():
 	wikiDict = pickle.load(open('wikis.pickle','rb'))
@@ -11,11 +11,9 @@ def main():
 		for sent in word_tokenize(text):
 			pos=pos_tag(sent)
 			for i,word in enumerate(sent):
-				print(lesk(sent,word,pos[i]))
-
-
-
-		tokens = [word for sent in sent_tokenize(value) for word in word_tokenize(sent)]
+				if pos[i][1]=='NNPS' or pos[i][1]=='NNP' or pos[i][1]=='NNS' or pos[i][1]=='NN':
+					if len(wn,synsets(word))>1: # TESTEN OF DEZE IF BIJ ALLE OR'S HIERBOVEN KAN ALS AND
+						print(lesk(sent,word,pos[i]))
 
 if __name__ == '__main__':
 	main()
