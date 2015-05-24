52b5b7861480788560b0acd57bd0432f7f93597d
Martbov <Mart@MacBook-Pro-van-Mart.local>
2015-05-24 11:34:15 +0200

    Changed disambiguifier.py

--- Week3/wikifier.py
+++ Week3/wikifier.py
@@ -1,19 +1,37 @@
-import os,pickle
-from posopener import *
+import os, pickle
+#from posopener import *
+
+def getwikis():
+	rootDir = "group1/"
+	wikiList=[]
+	for subdirs, dirs, files in os.walk(rootDir):
+		if len(files) > 5:
+			textFile = open(subdirs+"/"+files[5], 'r')
+			print(textFile)
+			rawText = textFile.readlines()
+			for token in rawText:
+				tokenspecs = token.split()
+				if len(tokenspecs) == 7:
+					wikiList.append(tokenspecs[6])
+	return wikiList
 
 def getText(wiki):
-	os.system("curl \"{}\"| grep \"<p>\" | sed \"s/<[^<]*>//g\" > wiki.tmp".format(wiki))
+	os.system("curl '{}'| grep '<p>' | sed 's/<[^<]*>//g' > wiki.tmp".format(wiki))
 	with open("wiki.tmp","r") as f:
 		os.system("rm wiki.tmp")
 		return f.read()
-wikiDict = {}
-wikis = getwikis()
-for wiki in set(wikis):
-	print(wiki)
-	text=getText(wiki)
-	wikiDict[wiki]=text
 
-with open('wikis.pickle','wb') as f:
+def main():
+	wikiDict = {}
+	wikis = getwikis()
+	print(wikis)
+	for wiki in set(wikis):
+		print(wiki)
+		text = getText(wiki)
+		wikiDict[wiki] = text
+
+	with open('wikis.pickle','wb') as f:
 		pickle.dump(wikiDict,f,protocol=0)
 
-
+if __name__ == '__main__':
+	main()
--- Week5/disambigufier.py
+++ Week5/disambigufier.py
@@ -6,14 +6,20 @@ from nltk.corpus import wordnet as wn
 def main():
 	wikiDict = pickle.load(open('wikis.pickle','rb'))
 	for value in wikiDict.values():
-		value=re.sub(r'\[[0-9]*\]',"",value)
-		text=sent_tokenize(value)
-		for sent in word_tokenize(text):
-			pos=pos_tag(sent)
-			for i,word in enumerate(sent):
-				if pos[i][1]=='NNPS' or pos[i][1]=='NNP' or pos[i][1]=='NNS' or pos[i][1]=='NN':
-					if len(wn,synsets(word))>1: # TESTEN OF DEZE IF BIJ ALLE OR'S HIERBOVEN KAN ALS AND
-						print(lesk(sent,word,pos[i]))
+		value = re.sub(r'\[[0-9]*\]',"",value)
+		text = sent_tokenize(value)
+		for sent in text:
+			tokenized = word_tokenize(sent)
+			pos = pos_tag(tokenized)
+			#print(pos[1][1])
+			for token, tag in pos:
+				if pos[1][1] == 'NNPS' or pos[1][1] == 'NNP' or pos[1][1] == 'NNS' or pos[1][1] == 'NN':
+					#print(sent, token, pos[1][1])
+					leskoutput = lesk(sent, token, pos[1][1])
+					if leskoutput != None:
+						print(leskoutput)
+				#if len(wn.synsets(word))>1: # TESTEN OF DEZE IF BIJ ALLE OR'S HIERBOVEN KAN ALS AND
+				#	print(lesk(sent,word,pos[i]))
 
 if __name__ == '__main__':
 	main()
