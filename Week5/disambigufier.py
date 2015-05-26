ff319ee62c656f8911b1a00122f7c756323079d5
Martbov <Mart@RN-145-97-167-226.eduroam.rug.nl>
2015-05-26 11:56:38 +0200

    Changed disamibgufier.py

--- Week5/disambigufier.py
+++ Week5/disambigufier.py
@@ -2,9 +2,11 @@ import pickle, nltk, re
 from nltk import pos_tag, word_tokenize, sent_tokenize
 from nltk.wsd import lesk
 from nltk.corpus import wordnet as wn
+from collections import Counter
 
 def main():
 	wikiDict = pickle.load(open('wikis.pickle','rb'))
+	c = Counter()
 	for value in wikiDict.values():
 		value = re.sub(r'\[[0-9]*\]',"",value)
 		text = sent_tokenize(value)
@@ -12,12 +14,16 @@ def main():
 			tokenized = word_tokenize(sent)
 			pos = pos_tag(tokenized)
 			for token, tag in pos:
-				if (tag == 'NNPS' or tag == 'NNP' or tag == 'NNS' or tag == 'NN') and len(wn.synsets(token))>1:
+				if (tag == 'NNPS' or tag == 'NNP' or tag == 'NNS' or tag == 'NN'):
 					tag = "n"
-					print(token)
-					print(lesk(sent, token, tag))
-					#if len(wn.synsets(token))>1: # TESTEN OF DEZE IF BIJ ALLE OR'S HIERBOVEN KAN ALS AND
-					#	print(lesk(sent, token, tag))
+					if len(wn.synsets(token, tag)) > 1:
+						print(token, lesk(sent, token, tag))
+						print("All possible senses:")
+						for ss in wn.synsets(token, tag):
+							print(ss, ss.definition())
+						
+	print(c)
+	
 
 if __name__ == '__main__':
 	main()
