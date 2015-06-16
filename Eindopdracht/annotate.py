256c65312e0fa7ad0990e638deee3a8a905e07d3
Martbov <Mart@MacBook-Pro-van-Mart.local>
2015-06-16 13:45:03 +0200

    Modified annotate.py for testset

--- Eindopdracht/annotate.py
+++ Eindopdracht/annotate.py
@@ -3,10 +3,10 @@
 from nltk.tag.stanford import NERTagger
 from collections import Counter, defaultdict
 from nltk.corpus import wordnet as wn
-import os, codecs, sys, urllib, json, pickle
+import os, codecs, sys, urllib.request, json, pickle
 from nltk.wsd import lesk
 from nltk import word_tokenize
-#from progressbar import ProgressBar
+from progressbar import ProgressBar
 
 #reload(sys)
 #sys.setdefaultencoding("utf-8")
@@ -14,9 +14,9 @@ from nltk import word_tokenize
 
 def createtraindata():
 	""" Creates the traindata for the NER Tagger """
-	newsHandler = codecs.open('development.set','r')
+	newsHandler = codecs.open('test.set','r')
 	#trainData = open('traindata_all.tsv', 'a')
-	#testData = open('testdata.tsv', 'a')
+	testData = open('testdata.tsv', 'w')
 	newsList = []
 	for line in newsHandler:
 		newsList.append(line)
@@ -25,10 +25,10 @@ def createtraindata():
 	#testPart = newsList[trainSplit:]
 	#createfiles(trainPart,trainData)
 	#createfiles(testPart,testData)
-	referenceDict = createfiles(newsList)#,trainData)
+	referenceDict = createfiles(newsList,testData)
 	newsHandler.close()
 	#trainData.close()
-	#testData.close()
+	testData.close()
 	return referenceDict
 	
 
@@ -42,68 +42,68 @@ def createfiles(Part,filename):
 		if len(lineList) > 6 and len(lineList[6]) == 3:
 			sentenceDict[str(lineList)] = [lineList[4], lineList[6]]
 			if docId != lineList[0]:
-				#filename.write('\n')
-				#filename.write(lineList[4]+'\t'+lineList[6]+'\n')
+				filename.write('\n')
+				filename.write(lineList[4]+'\t'+lineList[6]+'\n')
 				docId = lineList[0]		
-			#else:
-				#filename.write(lineList[4]+'\t'+lineList[6]+'\n')
+			else:
+				filename.write(lineList[4]+'\t'+lineList[6]+'\n')
 		elif len(lineList) > 5:
 			sentenceDict[str(lineList)] = [lineList[4]]
 			if docId != lineList[0]:
-				#filename.write('\n')
-				#filename.write(lineList[4]+'\t'+'O'+'\n')
+				filename.write('\n')
+				filename.write(lineList[4]+'\t'+'O'+'\n')
 				docId = lineList[0]		
-			#else:
-			#	filename.write(lineList[4]+'\t'+'O'+'\n')
+			else:
+				filename.write(lineList[4]+'\t'+'O'+'\n')
+		else:
+			sentenceDict[str(lineList)] = [lineList]
 	return sentenceDict
 
-def tagdata(testData):
+def tagdata(refDict):
 	""" Gives the data its NER Tags using our trained tagger """
 	tokens = []
 	testData = codecs.open('testdata.tsv', 'r')
-	for line in testData.values():
-		tokens.append(line[0])
+	for line in testData:
 		if len(line) > 1:
-			token = line.strip().split()
+			token = line.strip().split('\t')
 			tokens.append(token[0])
-	os.environ['JAVAHOME'] = "C:\Program Files\Java\jdk1.8.0_45/bin"
+	#os.environ['JAVAHOME'] = "C:\Program Files\Java\jdk1.8.0_45/bin"
 	path="ner"
 	classifier = "ner-pta.ser.gz"
 	jar = "stanford-ner.jar"
 	tagger = NERTagger(classifier, jar)
 	taggedText = tagger.tag(tokens)
 	for line in taggedText:
-		for value in testData.values():
-			if line[0][0] == value [0]:
-				value = line
-			
-	return taggedText, testData
+		for tup in line:
+			for key, value in refDict.items():
+				if tup[0] == value[0]:
+					refDict[key] = [tup[0],tup[1]]	
+	return taggedText, refDict
 
 
 def combineTags(taggedText):
 	""" Adds the tags to the testfile """
 	refDict=defaultdict(list)
 	sentList = []
-	for i, words in enumerate(taggedText):
-		if i != 0:
-			prevword = taggedText[i-1][0]
-			prevtag = taggedText[i-1][1]
-		else:
-			prevword=prevtag=''
-		if words[1] == prevtag:
-			newTuple = (newTuple[0]+' '+words[0],words[1])
-			sentList.pop()
-			sentList.append(newTuple)
-			refDict[i+1,words].append(newTuple)
+	for sentence in taggedText:
+		for i, words in enumerate(sentence):
+			if i != 0:
+				prevword = sentence[i-1][0]
+				prevtag = sentence[i-1][1]
+			else:
+				prevword=prevtag=''
+			if words[1] == prevtag:
+				newTuple = (newTuple[0]+' '+words[0],words[1])
+				sentList.pop()
+				sentList.append(newTuple)
+				refDict[i+1,words].append(newTuple)
 
-		else: 
-			newTuple = (words[0],words[1])
-			sentList.append(newTuple)
-			refDict[i+1,words].append(newTuple)
-	print(sorted(refDict.items()))
+			else: 
+				newTuple = (words[0],words[1])
+				sentList.append(newTuple)
+				refDict[i+1,words].append(newTuple)
 
 	for i, words in enumerate(sentList):
-		print(words)
 		if i != 0 and i < len(sentList)-1:
 			sent=sentList[i-1][0],words[0],sentList[i+1][0]
 			if words[1]!= 'O':
@@ -113,14 +113,11 @@ def combineTags(taggedText):
 					for value in refDict.values():
 						if value[0] == words and len(value) < 2:
 							value.append([mwords,leskDec,leskDec.definition()])
-					#print(leskDec,leskDec.definition())
 				else:
 					for ss in wn.synsets(mwords, 'n'):
-						#print(ss, ss.definition())
 						for value in refDict.values():
 							if value[0] == words and len(value) < 2:
 								value.append([mwords,ss, ss.definition()])
-	print(sorted(refDict.items()))
 	return refDict
 	
 	
@@ -133,15 +130,17 @@ def updatedevset(referenceDict):
 		for key, value in referenceDict.items():
 			if key == str(lineList):
 				if len(value) > 1:
-					if len(lineList) > 6:
+					if len(lineList) == 8:
 						lineList.pop()
 						lineList.pop()
-					lineList.append(value[1])#) = lineList+' '+value[1]+'\n'
+					elif len(lineList) == 7:
+						lineList.pop()
+					lineList.append(value[1])
 					taggedHandler.write(' '.join(lineList))
 					taggedHandler.write('\n')
 				else:
-					lineList = lineList+'\n'
-					taggedHandler.write(lineList)
+					taggedHandler.write(' '.join(lineList))
+					taggedHandler.write('\n')
 	newsHandler.close()
 	taggedHandler.close()
 
@@ -153,8 +152,10 @@ def listtags():
 		lineItems=line.strip().split()
 		if len(lineItems) > 6:
 			wordList.append((str(lineItems[4]),str(lineItems[6])))
-		else:
+		elif len(lineItems) > 3:
 			wordList.append((lineItems[4],'O'))
+		else:
+			pass
 	taggedHandler.close()
 	#print([word for word in wordList])
 	return wordList
@@ -163,10 +164,10 @@ def getwikiurls(refDict):
 	""" Searches for the wikipedia links """
 	#pbar = ProgressBar()
 	wikiDict={}
-	for key, value in pbar(refDict.items()):
+	for key, value in refDict.items():
 		if value[0][1] != 'O' or value[0][1] != '-':
 			if len(value) > 1:
-				wikiresults = urllib.urlopen("http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch="+value[1][0]+"&format=json").read().decode('utf-8')
+				wikiresults = urllib.request.urlopen("http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch="+value[1][0]+"&format=json").read().decode('utf-8')
 				wikisuggs = json.loads(wikiresults)
 				for query in wikisuggs:
 					for search in wikisuggs[query]:
@@ -175,8 +176,7 @@ def getwikiurls(refDict):
 							for article in wikisuggs[query][search]:
 								if "snippet" in article:
 									x=0
-									for word in article.values()[3]:
-										
+									for word in article.values():
 										if str(word) in str(value[1][2]):
 											x+=1
 									decisionList.append((x, article.values()[0]))
@@ -186,11 +186,11 @@ def getwikiurls(refDict):
 								wikiDict[key] = wikiLink
 	return wikiDict
 
-def addurls():
+def addurls(urls):
 	""" Adds the wikipedia links to the file """
-	urls = pickle.load(open('wikiurls.pickle','rb'))
+	#urls = pickle.load(open('wikiurls.pickle','rb'))
 	taggedHandler = open('nertagged.set','r')
-	wikifiedHandler = open('wikitagged.set','a')
+	wikifiedHandler = open('wikitagged.set','w')
 	for i, line in enumerate(taggedHandler):
 		lineList=line.strip().split()
 		if len(lineList) > 6 and (i+1,(lineList[4],lineList[6])) in urls.keys():
@@ -229,7 +229,7 @@ def wikiexpander():
 			if line[6] == prevTag:
 				line.append(prevWiki)
 			else:
-				link = 'http://en.wikipedia.org/'+line[4]+',1'
+				link = 'http://en.wikipedia.org/wiki/'+line[4]+',1'
 				line.append(link)
 			experiment.write(' '.join(line))
 			experiment.write('\n')
@@ -254,25 +254,16 @@ def reverseTagset():
 	endresult.close()
 
 
-def cleantagset(tagset):
-
-	taggedHandler.write(' '.join(lineList))
-	taggedHandler.write('\n')
-	taggedHandler.close()
-
 if __name__ == '__main__':
 	referenceDict = createtraindata()
 	#os.popen("java -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -prop ptatagger.prop")
 	taggedText, referenceDict = tagdata(referenceDict)
 	updatedevset(referenceDict)
-	taggedText= listtags()
-	taggedText, referenceDict = tagdata(referenceDict)
-	tagset = updatedevset(referenceDict)
-	cleantagset()
-	decidedSs=combineTags(taggedText)
+	#taggedText = listtags()
+	decidedSs = combineTags(taggedText)
 	urls=getwikiurls(decidedSs)
 	with open('wikiurls.pickle','wb') as f:
 		pickle.dump(urls,f)
-	addurls()
+	addurls(urls)
 	wikiexpander()
 	reverseTagset()
